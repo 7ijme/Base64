06e880bc1cd9dd3e36b878d645c535ec5d0602d7
7ijme <tijmevh@gmail.com>
2021-08-27 13:51:05 +0200

    Update

---
+++ index.js
@@ -0,0 +1,62 @@
+const base64Characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".split("");
+
+function encode(string) {
+	let binaryString = "";
+	for (const char of string) {
+		let plus = char.charCodeAt(0).toString(2);
+		plus = `${plus.length < 8 ? "0".repeat(8 - plus.length) : ""}${plus}`;
+		binaryString += plus;
+	}
+	let index = 1;
+	let currentString = "";
+	let newString = "";
+	for (const character of binaryString) {
+		currentString += character;
+		if (index % 6 == 0) {
+			const base64Character = base64Characters[parseInt(currentString, 2)];
+			newString += base64Character;
+			currentString = "";
+		}
+
+		index++;
+	}
+	if (currentString.length > 0) {
+		let addEquals = "";
+		if (currentString.length == 4) addEquals = "=";
+		if (currentString.length == 2) addEquals = "==";
+		currentString = `${currentString.length - 6 ? "0".repeat(6 - currentString.length) : ""}${currentString}`;
+		const base64Character = base64Characters[parseInt(currentString, 2)];
+		newString += base64Character + addEquals;
+		currentString = "";
+	}
+	return newString;
+}
+
+function decode(string) {
+	let binaryString = "";
+	for (const character of string) {
+		const base64Binary = base64Characters.indexOf(character).toString(2);
+		binaryString += `${base64Binary.length < 6 ? "0".repeat(6 - base64Binary.length) : ""}${base64Binary}`;
+	}
+
+	let currentString = "";
+	let newString = "";
+	let index = 1;
+	for (const character of binaryString) {
+		currentString += character;
+		if (index % 8 == 0) {
+			const base64Character = String.fromCharCode(parseInt(currentString, 2));
+			newString += base64Character;
+			currentString = "";
+		}
+
+		index++;
+	}
+	return newString;
+}
+
+module.exports = {
+	encode,
+	decode,
+	base64Characters,
+};
